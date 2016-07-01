5e67c9d7d07096dd6b8a60d97f5a1ceed77131c5
Jeff King <peff@peff.net>
2016-07-01 02:06:02 -0400

    common-main: call sanitize_stdfds()
    This is setup that should be done in every program for
    safety, but we never got around to adding it everywhere (so
    builtins benefited from the call in git.c, but any external
    commands did not). Putting it in the common main() gives us
    this safety everywhere.
    
    Note that the case in daemon.c is a little funny. We wait
    until we know whether we want to daemonize, and then either:
    
     - call daemonize(), which will close stdio and reopen it to
       /dev/null under the hood
    
     - sanitize_stdfds(), to fix up any odd cases
    
    But that is way too late; the point of sanitizing is to give
    us reliable descriptors on 0/1/2, and we will already have
    executed code, possibly called die(), etc. The sanitizing
    should be the very first thing that happens.
    
    With this patch, git-daemon will sanitize first, and can
    remove the call in the non-daemonize case. It does mean that
    daemonize() may just end up closing the descriptors we
    opened, but that's not a big deal (it's not wrong to do so,
    nor is it really less optimal than the case where our parent
    process redirected us from /dev/null ahead of time).
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- common-main.c
+++ common-main.c
@@ -33,6 +33,13 @@ int main(int argc, const char **argv)
 
 	git_setup_gettext();
 
+	/*
+	 * Always open file descriptors 0/1/2 to avoid clobbering files
+	 * in die().  It also avoids messing up when the pipes are dup'ed
+	 * onto stdin/stdout/stderr in the child processes we spawn.
+	 */
+	sanitize_stdfds();
+
 	argv[0] = git_extract_argv0_path(argv[0]);
 
 	restore_sigpipe_to_default();
