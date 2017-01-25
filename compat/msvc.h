13b4173a28ad16f31d1699165dc41ab89a47bd70
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-01-25 12:17:28 +0100

    base: cherry-pick f851a9f740 onto 149e489d18
    This commit helps to teach `git rerere` to resolve merge 
    conflicts when cherry-picking:
    
    	f851a9f740 (msvc: define ftello(), 2016-06-03)

--- compat/msvc.h
+++ compat/msvc.h
@@ -24,9 +24,12 @@ static __inline int strcasecmp (const char *s1, const char *s2)
 
 #undef ERROR
 
+#ifdef _MSC_VER
 typedef int sigset_t;
 /* open for reading, writing, or both (not in fcntl.h) */
 #define O_ACCMODE     (_O_RDONLY | _O_WRONLY | _O_RDWR)
+
+#endif
 
 #include "compat/mingw.h"
 
