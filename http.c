6294270b97454b44b2db7c8ffb3b092820f31a91
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-03-17 13:36:52 +0100

    base: cherry-pick de4b3c5efa onto ef945ac86a
    This commit helps to teach `git rerere` to resolve merge 
    conflicts when cherry-picking:
    
    	de4b3c5efa (http(s): automatically try NTLM authentication first, 2016-12-09)

--- http.c
+++ http.c
@@ -109,7 +109,7 @@ static int curl_save_cookies;
 struct credential http_auth = CREDENTIAL_INIT;
 static int http_proactive_auth;
 static const char *user_agent;
-static int curl_empty_auth = -1;
+static int curl_empty_auth;
 
 enum http_follow_config http_follow_config = HTTP_FOLLOW_INITIAL;
 
@@ -125,14 +125,6 @@ static struct credential cert_auth = CREDENTIAL_INIT;
 static int ssl_cert_password_required;
 #ifdef LIBCURL_CAN_HANDLE_AUTH_ANY
 static unsigned long http_auth_methods = CURLAUTH_ANY;
-static int http_auth_methods_restricted;
-/* Modes for which empty_auth cannot actually help us. */
-static unsigned long empty_auth_useless =
-	CURLAUTH_BASIC
-#ifdef CURLAUTH_DIGEST_IE
-	| CURLAUTH_DIGEST_IE
-#endif
-	| CURLAUTH_DIGEST;
 #endif
 
 static struct curl_slist *pragma_header;
@@ -271,10 +263,10 @@ static int http_options(const char *var, const char *value, void *cb)
 	if (!strcmp("http.sslversion", var))
 		return git_config_string(&ssl_version, var, value);
 	if (!strcmp("http.sslcert", var))
-		return git_config_pathname(&ssl_cert, var, value);
+		return git_config_string(&ssl_cert, var, value);
 #if LIBCURL_VERSION_NUM >= 0x070903
 	if (!strcmp("http.sslkey", var))
-		return git_config_pathname(&ssl_key, var, value);
+		return git_config_string(&ssl_key, var, value);
 #endif
 #if LIBCURL_VERSION_NUM >= 0x070908
 	if (!strcmp("http.sslcapath", var))
@@ -341,10 +333,7 @@ static int http_options(const char *var, const char *value, void *cb)
 		return git_config_string(&user_agent, var, value);
 
 	if (!strcmp("http.emptyauth", var)) {
-		if (value && !strcmp("auto", value))
-			curl_empty_auth = -1;
-		else
-			curl_empty_auth = git_config_bool(var, value);
+		curl_empty_auth = git_config_bool(var, value);
 		return 0;
 	}
 
@@ -393,37 +382,10 @@ static int http_options(const char *var, const char *value, void *cb)
 	return git_default_config(var, value, cb);
 }
 
-static int curl_empty_auth_enabled(void)
-{
-	if (curl_empty_auth >= 0)
-		return curl_empty_auth;
-
-#ifndef LIBCURL_CAN_HANDLE_AUTH_ANY
-	/*
-	 * Our libcurl is too old to do AUTH_ANY in the first place;
-	 * just default to turning the feature off.
-	 */
-#else
-	/*
-	 * In the automatic case, kick in the empty-auth
-	 * hack as long as we would potentially try some
-	 * method more exotic than "Basic" or "Digest".
-	 *
-	 * But only do this when this is our second or
-	 * subsequent request, as by then we know what
-	 * methods are available.
-	 */
-	if (http_auth_methods_restricted &&
-	    (http_auth_methods & ~empty_auth_useless))
-		return 1;
-#endif
-	return 0;
-}
-
 static void init_curl_http_auth(CURL *result)
 {
 	if (!http_auth.username || !*http_auth.username) {
-		if (curl_empty_auth_enabled())
+		if (curl_empty_auth)
 			curl_easy_setopt(result, CURLOPT_USERPWD, ":");
 		return;
 	}
@@ -1117,7 +1079,7 @@ struct active_request_slot *get_active_slot(void)
 #ifdef LIBCURL_CAN_HANDLE_AUTH_ANY
 	curl_easy_setopt(slot->curl, CURLOPT_HTTPAUTH, http_auth_methods);
 #endif
-	if (http_auth.password || curl_empty_auth_enabled())
+	if (http_auth.password || curl_empty_auth)
 		init_curl_http_auth(slot->curl);
 
 	return slot;
@@ -1385,10 +1347,6 @@ static int handle_curl_result(struct slot_results *results)
 		} else {
 #ifdef LIBCURL_CAN_HANDLE_AUTH_ANY
 			http_auth_methods &= ~CURLAUTH_GSSNEGOTIATE;
-			if (results->auth_avail) {
-				http_auth_methods &= results->auth_avail;
-				http_auth_methods_restricted = 1;
-			}
 #endif
 			return HTTP_REAUTH;
 		}
@@ -1768,9 +1726,6 @@ static int http_request_reauth(const char *url,
 			       struct http_get_options *options)
 {
 	int ret = http_request(url, result, target, options);
-
-	if (ret != HTTP_OK && ret != HTTP_REAUTH)
-		return ret;
 
 	if (options && options->effective_url && options->base_url) {
 		if (update_url_from_redirect(options->base_url,
