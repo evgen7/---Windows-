1318425e04b7582f6167c661fc2d3c0f1f3a5b27
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-03-30 02:49:32 +0200

    base: cherry-pick e920a5be8f onto d9dd990281
    This commit helps to teach `git rerere` to resolve merge 
    conflicts when cherry-picking:
    
    	e920a5be8f (fixup! Merge pull request #964 from jeffhostetler/jeffhostetler/memihash_perf, 2017-03-27)

--- preload-index.c
+++ preload-index.c
@@ -6,8 +6,7 @@
 #include "dir.h"
 
 #ifdef NO_PTHREADS
-static void preload_index(struct index_state *index,
-			  const struct pathspec *pathspec)
+void preload_index(struct index_state *index, const struct pathspec *pathspec)
 {
 	; /* nothing */
 }
@@ -47,8 +46,6 @@ static void *preload_thread(void *_data)
 		struct cache_entry *ce = *cep++;
 		struct stat st;
 
-		precompute_istate_hashes(ce);
-
 		if (ce_stage(ce))
 			continue;
 		if (S_ISGITLINK(ce->ce_mode))
@@ -71,8 +68,7 @@ static void *preload_thread(void *_data)
 	return NULL;
 }
 
-static void preload_index(struct index_state *index,
-			  const struct pathspec *pathspec)
+void preload_index(struct index_state *index, const struct pathspec *pathspec)
 {
 	int threads, i, work, offset;
 	struct thread_data data[MAX_PARALLEL];
@@ -88,6 +84,7 @@ static void preload_index(struct index_state *index,
 	offset = 0;
 	work = DIV_ROUND_UP(index->cache_nr, threads);
 	memset(&data, 0, sizeof(data));
+	enable_fscache(1);
 	for (i = 0; i < threads; i++) {
 		struct thread_data *p = data+i;
 		p->index = index;
@@ -104,6 +101,7 @@ static void preload_index(struct index_state *index,
 		if (pthread_join(p->pthread, NULL))
 			die("unable to join threaded lstat");
 	}
+	enable_fscache(0);
 }
 #endif
 
