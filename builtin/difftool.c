c183fbc992f042c840278a707ce4a2035a4dce1b
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-05-02 12:04:02 +0200

    base: cherry-pick 56dba3d40a onto 2f6456b2ff
    This commit helps to teach `git rerere` to resolve merge 
    conflicts when cherry-picking:
    
    	56dba3d40a (difftool: close file descriptors after reading, 2017-04-17)

--- builtin/difftool.c
+++ builtin/difftool.c
@@ -226,7 +226,6 @@ static void changed_files(struct hashmap *result, const char *index_path,
 		hashmap_entry_init(entry, strhash(buf.buf));
 		hashmap_add(result, entry);
 	}
-	fclose(fp);
 	if (finish_command(&diff_files))
 		die("diff-files did not exit properly");
 	strbuf_release(&index_env);
@@ -440,10 +439,8 @@ static int run_dir_diff(const char *extcmd, int symlinks, const char *prefix,
 		}
 
 		if (lmode && status != 'C') {
-			if (checkout_path(lmode, &loid, src_path, &lstate)) {
-				ret = error("could not write '%s'", src_path);
-				goto finish;
-			}
+			if (checkout_path(lmode, &loid, src_path, &lstate))
+				return error("could not write '%s'", src_path);
 		}
 
 		if (rmode && !S_ISLNK(rmode)) {
@@ -459,12 +456,9 @@ static int run_dir_diff(const char *extcmd, int symlinks, const char *prefix,
 			hashmap_add(&working_tree_dups, entry);
 
 			if (!use_wt_file(workdir, dst_path, &roid)) {
-				if (checkout_path(rmode, &roid, dst_path,
-						  &rstate)) {
-					ret = error("could not write '%s'",
-						    dst_path);
-					goto finish;
-				}
+				if (checkout_path(rmode, &roid, dst_path, &rstate))
+					return error("could not write '%s'",
+						     dst_path);
 			} else if (!is_null_oid(&roid)) {
 				/*
 				 * Changes in the working tree need special
@@ -479,12 +473,10 @@ static int run_dir_diff(const char *extcmd, int symlinks, const char *prefix,
 						ADD_CACHE_JUST_APPEND);
 
 				add_path(&rdir, rdir_len, dst_path);
-				if (ensure_leading_directories(rdir.buf)) {
-					ret = error("could not create "
-						    "directory for '%s'",
-						    dst_path);
-					goto finish;
-				}
+				if (ensure_leading_directories(rdir.buf))
+					return error("could not create "
+						     "directory for '%s'",
+						     dst_path);
 				add_path(&wtdir, wtdir_len, dst_path);
 				if (symlinks) {
 					if (symlink(wtdir.buf, rdir.buf)) {
@@ -505,15 +497,13 @@ static int run_dir_diff(const char *extcmd, int symlinks, const char *prefix,
 		}
 	}
 
-	fclose(fp);
-	fp = NULL;
 	if (finish_command(&child)) {
 		ret = error("error occurred running diff --raw");
 		goto finish;
 	}
 
 	if (!i)
-		goto finish;
+		return 0;
 
 	/*
 	 * Changes to submodules require special treatment.This loop writes a
@@ -636,9 +626,6 @@ static int run_dir_diff(const char *extcmd, int symlinks, const char *prefix,
 		exit_cleanup(tmpdir, rc);
 
 finish:
-	if (fp)
-		fclose(fp);
-
 	free(lbase_dir);
 	free(rbase_dir);
 	strbuf_release(&ldir);
