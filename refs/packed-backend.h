02be88ba9ba867bc041417e225111ea092baf89e
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-08-28 13:39:03 +0200

    base: cherry-pick 77a185e893 onto 2ff1b3d401
    This commit helps to teach `git rerere` to resolve merge 
    conflicts when cherry-picking:
    
    	77a185e893 (packed_refs_lock(): function renamed from lock_packed_refs(), 2017-06-23)

--- refs/packed-backend.h
+++ refs/packed-backend.h
@@ -6,13 +6,10 @@ struct ref_store *packed_ref_store_create(const char *path,
 
 /*
  * Lock the packed-refs file for writing. Flags is passed to
- * hold_lock_file_for_update(). Return 0 on success. On errors, write
- * an error message to `err` and return a nonzero value.
+ * hold_lock_file_for_update(). Return 0 on success. On errors, set
+ * errno appropriately and return a nonzero value.
  */
-int packed_refs_lock(struct ref_store *ref_store, int flags, struct strbuf *err);
-
-void packed_refs_unlock(struct ref_store *ref_store);
-int packed_refs_is_locked(struct ref_store *ref_store);
+int lock_packed_refs(struct ref_store *ref_store, int flags);
 
 void add_packed_ref(struct ref_store *ref_store,
 		    const char *refname, const struct object_id *oid);
