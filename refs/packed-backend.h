0292b4aaadb6dccceb72fbba387d5abb43a89219
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-09-08 12:57:40 +0200

    base: cherry-pick 5d70cafb50 onto 8667924fad
    This commit helps to teach `git rerere` to resolve merge 
    conflicts when cherry-picking:
    
    	5d70cafb50 (packed_ref_store: make class into a subclass of `ref_store`, 2017-06-23)

--- refs/packed-backend.h
+++ refs/packed-backend.h
@@ -1,26 +1,33 @@
 #ifndef REFS_PACKED_BACKEND_H
 #define REFS_PACKED_BACKEND_H
 
-/*
- * Support for storing references in a `packed-refs` file.
- *
- * Note that this backend doesn't check for D/F conflicts, because it
- * doesn't care about them. But usually it should be wrapped in a
- * `files_ref_store` that prevents D/F conflicts from being created,
- * even among packed refs.
- */
+struct packed_ref_store *packed_ref_store_create(
+		const char *path, unsigned int store_flags);
 
-struct ref_store *packed_ref_store_create(const char *path,
-					  unsigned int store_flags);
+int packed_read_raw_ref(struct packed_ref_store *refs,
+			const char *refname, unsigned char *sha1,
+			struct strbuf *referent, unsigned int *type);
+
+int packed_peel_ref(struct packed_ref_store *refs,
+		    const char *refname, unsigned char *sha1);
+
+struct ref_iterator *packed_ref_iterator_begin(
+		struct packed_ref_store *refs,
+		const char *prefix, unsigned int flags);
 
 /*
  * Lock the packed-refs file for writing. Flags is passed to
- * hold_lock_file_for_update(). Return 0 on success. On errors, write
- * an error message to `err` and return a nonzero value.
+ * hold_lock_file_for_update(). Return 0 on success. On errors, set
+ * errno appropriately and return a nonzero value.
  */
-int packed_refs_lock(struct ref_store *ref_store, int flags, struct strbuf *err);
+int lock_packed_refs(struct packed_ref_store *refs, int flags);
 
-void packed_refs_unlock(struct ref_store *ref_store);
-int packed_refs_is_locked(struct ref_store *ref_store);
+void add_packed_ref(struct packed_ref_store *refs,
+		    const char *refname, const struct object_id *oid);
+
+int commit_packed_refs(struct packed_ref_store *refs);
+
+int repack_without_refs(struct packed_ref_store *refs,
+			struct string_list *refnames, struct strbuf *err);
 
 #endif /* REFS_PACKED_BACKEND_H */
