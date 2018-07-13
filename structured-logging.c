8e56c0412810b35138d776e6b13e45ef1558d90d
Jeff Hostetler <jeffhost@microsoft.com>
2018-07-13 16:55:58 +0000

    structured-logging: add STRUCTURED_LOGGING=1 to Makefile
    Teach the Makefile to take STRUCTURED_LOGGING=1 variable to
    compile in/out structured logging feature.
    
    Signed-off-by: Jeff Hostetler <jeffhost@microsoft.com>

---
+++ structured-logging.c
@@ -0,0 +1,9 @@
+#if !defined(STRUCTURED_LOGGING)
+/*
+ * Structured logging is not available.
+ * Stub out all API routines.
+ */
+
+#else
+
+#endif
---
+++ structured-logging.h
@@ -0,0 +1,13 @@
+#ifndef STRUCTURED_LOGGING_H
+#define STRUCTURED_LOGGING_H
+
+#if !defined(STRUCTURED_LOGGING)
+/*
+ * Structured logging is not available.
+ * Stub out all API routines.
+ */
+
+#else
+
+#endif /* STRUCTURED_LOGGING */
+#endif /* STRUCTURED_LOGGING_H */
